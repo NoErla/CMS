9658a792f729829e6948854d7e00834fbbcec87a
youzipi <blake_12@qq.com>
2015-11-18 18:44:32 +0800

    update pom.xml

--- src/main/java/com/shishuo/cms/service/AdminService.java
+++ src/main/java/com/shishuo/cms/service/AdminService.java
@@ -6,17 +6,6 @@
 
 package com.shishuo.cms.service;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.shishuo.cms.constant.SystemConstant;
 import com.shishuo.cms.dao.AdminDao;
 import com.shishuo.cms.entity.Admin;
@@ -25,6 +14,15 @@ import com.shishuo.cms.entity.vo.PageVo;
 import com.shishuo.cms.exception.AuthException;
 import com.shishuo.cms.util.AuthUtils;
 import com.shishuo.cms.util.PropertyUtils;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 /**
  * 管理员
